3a04ee07d07b3066de595b952eaeb55254b35376
Salman <salzahir18@gmail.com>
2025-06-15 20:05:12 -0500

    Refactor Waldo controller to use new model with multiple targets

--- server/src/controllers/waldo.ts
+++ server/src/controllers/waldo.ts
@@ -1,17 +1,5 @@
 import { Request, Response } from "express";
-
-class Waldo {
-    constructor(public character: string, public posX: number, public posY: number, public marginError: number) {}
-    isWaldoFound(postX: number, postY: number): boolean {
-        const xInRange = postX >= this.posX - this.marginError && postX <= this.posX + this.marginError;
-        const yInRange = postY >= this.posY - this.marginError && postY <= this.posY + this.marginError;
-        return xInRange && yInRange;
-    }
-}
-
-const data = {
-    Waldo: new Waldo("White Turban Guy", 0.2540, 0.2533, 0.05)
-}
+import waldoTargets from "../models/waldo";
 
 function handleHome(req: Request, res: Response): void {
   res.status(200).json({ message: "Welcome to the Waldo API!" });
@@ -24,7 +12,7 @@ function handlePosts(req: Request, res: Response): void {
 async function handleWaldoCheck(req: Request, res: Response): Promise<void> {
   try {
     const { postX, postY } = req.body;
-    for (const waldo of Object.values(data)) {
+    for (const waldo of Object.values(waldoTargets)) {
       if (waldo.isWaldoFound(postX, postY)) {
         res.status(200).json({ found: true, character: waldo.character });
         return;
